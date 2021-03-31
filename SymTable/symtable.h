6fcc4a6d3c8c2d9f05cc5e73bc79912aa40cafac
Nickolas Casalinuovo <nickolascasalinuovo@dynamic-oit-vapornet100-10-9-66-141.princeton.edu>
2021-03-31 02:58:01 -0400

    Debug

--- SymTable/symtable.h
+++ SymTable/symtable.h
@@ -28,53 +28,55 @@ SymTable_T SymTable_new(void);
 /*   then the function must leave oSymTable unchanged and             */
 /*   return 0 (FALSE).                                                */
 /*--------------------------------------------------------------------*/
-int SymTable_put(SymTable_T oSymTable, const char *pcKey, const void *pvValue);
+int SymTable_put(SymTable_T oSymTable, const char *pcKey, 
+    const void *pvValue);
 
 
 /*--------------------------------------------------------------------*/
-/* SymTable_free takes a SymTable_T as an argument and clears all     */
-/* associated memory in the heap.                                     */
+/* SymTable_free takes a SymTable_T (oSymTable) as an argument and    */
+/* clears all associated memory in the heap.                          */
 /*--------------------------------------------------------------------*/
 void SymTable_free(SymTable_T oSymTable);
 
 /*--------------------------------------------------------------------*/
-/* SymTable_getLength takes a SymTable_T as an argument and returns   */
-/* the amount of bindings within the symbol table.                    */
+/* SymTable_getLength takes a SymTable_T (oSymTable) as an argument   */
+/* and returns the amount of bindings within the symbol table.        */
 /*--------------------------------------------------------------------*/
 size_t SymTable_getLength(SymTable_T oSymTable);
 
 /*--------------------------------------------------------------------*/
-/**/
-/**/
-/**/
+/* Symtable_replace takes a Symtable_T (oSymTable) and seaches for a  */
+/* pcKey. If the pcKey is found, set its value to pvValue and return  */
+/* old pvValue. If pcKey is not found, return NULL.                   */
 /*--------------------------------------------------------------------*/
-void *SymTable_replace(SymTable_T oSymTable, const char *pcKey, const void *pvValue);
+void *SymTable_replace(SymTable_T oSymTable, const char *pcKey,
+ const void *pvValue);
 
 /*--------------------------------------------------------------------*/
-/**/
-/**/
-/**/
+/* SymTable_contains takes a Symtable_T (oSymTable) and seaches for a */
+/* pcKey. If the pcKey is found, return 1. If pcKey is not found,     */
+/* return NULL.                                                       */
 /*--------------------------------------------------------------------*/
 int SymTable_contains(SymTable_T oSymTable, const char *pcKey);
 
 /*--------------------------------------------------------------------*/
-/**/
-/**/
-/**/
+/* SymTable_contains takes a Symtable_T (oSymTable) and seaches for a */
+/* pcKey. If the pcKey is found, return its pvValue. If pcKey is not  */
+/* found, return NULL.                                                */
 /*--------------------------------------------------------------------*/
 void *SymTable_get(SymTable_T oSymTable, const char *pcKey);
 
 /*--------------------------------------------------------------------*/
-/**/
-/**/
-/**/
+/* SymTable_contains takes a Symtable_T (oSymTable) and seaches for a */
+/* pcKey. If the pcKey is found, remove pcKey and return its pvValue. */
+/* If pcKey is not found, return NULL.                                */
 /*--------------------------------------------------------------------*/
 void *SymTable_remove(SymTable_T oSymTable, const char *pcKey);
 
 /*--------------------------------------------------------------------*/
-/**/
-/**/
-/**/
+/* SymTable_map takes a Symtable_T (oSymTable) and applies a function,*/
+/* pfApply, to every element. pfApply takes pcKey, pvValue, and an    */
+/* additional keyword, pvExtra                                        */
 /*--------------------------------------------------------------------*/
 void SymTable_map(SymTable_T oSymTable, 
     void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra), 
